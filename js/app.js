c814244323bd0fcfd64f4c369021ee4a32125dd4
Rohit Sharma <adamendeve@gmail.com>
2015-04-14 23:38:32 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-//Set up module
+//RedditApp Module
 var redditApp = angular.module('redditApp', ['ngRoute']);
 
 
@@ -14,97 +14,75 @@ redditApp.controller('mainController', ['$scope', '$http', '$location', '$rootEl
     $scope.redditify = function(){
         $scope.sr;
         $scope.success = false;
-    $http.get('http://www.reddit.com/r/' + $scope.sr + '.json?count=' + $scope.count + '&limit=25' + $scope.next)
+    	$http.get('http://www.reddit.com/r/' + $scope.sr + '.json?count=' + $scope.count + '&limit=25' + $scope.next)
    
         .success(function (data){
            
-        $scope.success = true;
-        $scope.subreddits = [];
-      
-            $scope.next = null;
-            for(var i = 0; i < data.data.children.length; i++)
-            { 
+	        $scope.success = true;
+	        $scope.subreddits = [];
+        	$scope.next = null;
+        	
+	        for(var i = 0; i < data.data.children.length; i++)
+	       	{ 
                 //Hide placeholder div
                 //Show page nav.
-                if(data.data.children.length > 0)
-                {
-                    $('.previouspage, .nextpage').removeClass('hidden');
-                    $('#placeholderdiv').addClass('hidden');
-                }
-                //Dealing with .gifs
-                var str =  data.data.children[i].data.url;
-                var rest = str.substring(0, str.lastIndexOf("/") + 1);
-		        var last = str.substring(str.lastIndexOf("/") + 1, str.length);
-                //console.log(last);    
-                var split = last.split(".");
-                var fileformat = split[1];
-                    if(fileformat == "gifv")
-                        data.data.children[i].data.url = "http://" + data.data.children[i].data.domain + "/" + split[0] + ".gif";
-                    if(fileformat == "jpg" || fileformat == "png" || fileformat == "bmp" || fileformat =="tif")
-                    {
-$scope.subreddits.push(data.data.children[i].data);
-
-                    } 
-                           // console.log(data);
-                            //console.log(data.data.after + "<-After");
-               
+                	if(data.data.children.length > 0)
+                	{
+	                    $('.previouspage, .nextpage').removeClass('hidden');
+	                    $('#placeholderdiv').addClass('hidden');
+                	}
+                	
+	                //Dealing with .gifs
+	        	var str =  data.data.children[i].data.url;
+	                var rest = str.substring(0, str.lastIndexOf("/") + 1);
+			var last = str.substring(str.lastIndexOf("/") + 1, str.length);
+	                  
+	                var split = last.split(".");
+	                var fileformat = split[1];
+	                
+                //Check if .gifv format. Not supporting that.
+                    	if(fileformat == "gifv")
+                        	data.data.children[i].data.url = "http://" + data.data.children[i].data.domain + "/" + split[0] + ".gif";
+                    	if(fileformat == "jpg" || fileformat == "png" || fileformat == "bmp" || fileformat =="tif")
+                    	{
+				$scope.subreddits.push(data.data.children[i].data);
+                    	} 
+                         
                 
                          $scope.after = data.data.after;
                          $scope.before = data.data.before;
-                                                                                                       //$scope.permalinks.push(data.data.children[i].data.permalink);
-
                          $scope.subreddit = data.data.children[i].data.subreddit;
             }   
-        
-        
-       
-        
-    });
+        });
     
-    
-        
-    }
-    
-
-
-$scope.resetPage= function(){
-    $scope.page = 1;
-}
-
-
-]
-
+     } 
 
 }]);
-    
-    //Code for the info qtip
+
+//Directive for showing the info qtip.
 redditApp.directive('ngHover', function() {
   return {
-      
     link: function(scope, element) {
-   element.qtip({
-         position: {
-        my: 'bottom center', 
-        at: 'top center',
-        target: element,
-        adjust: { mouse: false } 
-    },
-           style: { classes: 'qtip-youtube' },
-
-       
-       content:
-       {
-   text: scope.subreddit.title
-
-       }
+	    element.qtip
+	    ({
+	        position: {
+	        my: 'bottom center', // Position it where the click was...
+	        at: 'top center',
+	        target: element,
+	        adjust: { mouse: false } // ...but don't follow the mouse
+	        },
+	        style: { classes: 'qtip-youtube' },
+	       content:
+	        {
+	       	     text: scope.subreddit.title
+		}
+	
+	   })
    
-   
-   })
-   
-   element.on('click', function(){
-   	$('#myModal p a').remove();
+     	element.on('click', function(){
+		
         $('.modal-body', '.modal-title').empty();
-        
+        $('#myModal p a').remove();
         $('.modal-title').append('<a href="http://reddit.com' + scope.subreddit.permalink + '" target="_blank">' + scope.subreddit.title + '</a>');
         $('.modal-body').append('<img id="mimg" src="' + scope.subreddit.url + '">');
 
@@ -119,28 +97,24 @@ redditApp.directive('ngHover', function() {
     }
   }
 });
+//Directive ends
 
 //Next Page 
 redditApp.directive('nextPage', function(){
-
-    return {
+     return {
+          link: function(scope, element) {
+               element.on('click', function() {
+	            scope.next = "&after=" + scope.after;
+	            scope.count = scope.count + 25;
+	                
+	            scope.redditify();
+               });
+        
+        
+        
+           }    
     
-        link: function(scope, element){
-        
-            element.on('click', function(){
-            scope.next = "&after=" + scope.after;
-            scope.count = scope.count + 25;
-                
-            scope.redditify();
-            
-            
-            });
-        
-        
-        
-        }    
-    
-    }
+     }
 
 
 
@@ -150,9 +124,7 @@ redditApp.directive('nextPage', function(){
 redditApp.directive('previousPage', function(){
 
     return {
-    
-        link: function(scope, element){
-        
+         link: function(scope, element){
             element.on('click', function(){
             scope.next = "&before=" + scope.before;
             scope.count = scope.count - 25;
@@ -164,7 +136,7 @@ redditApp.directive('previousPage', function(){
         
         
         
-        }    
+         }    
     
     }
 
